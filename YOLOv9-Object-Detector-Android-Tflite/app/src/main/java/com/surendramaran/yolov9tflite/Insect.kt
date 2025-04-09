c56cdb1c78aedd6c9d46978ec83b22f35d344ec1
cuong huynh <ch26092003@gmail.com>
2025-04-09 20:47:26 +0700

    them listview con trung

---
+++ YOLOv9-Object-Detector-Android-Tflite/app/src/main/java/com/surendramaran/yolov9tflite/Insect.kt
@@ -0,0 +1,4 @@
+data class Insect(
+    val name: String,
+    val imageResId: Int // hoặc bạn có thể dùng String URL nếu ảnh từ Supabase
+)
---
+++ YOLOv9-Object-Detector-Android-Tflite/app/src/main/java/com/surendramaran/yolov9tflite/InsectAdapter.kt
@@ -0,0 +1,4 @@
+package com.surendramaran.yolov9tflite
+
+class InsectAdapter {
+}
